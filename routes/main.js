32cf65cc2737c285a21c44f442aba525e6c84586
Roeland De Smedt <roeland.desmedt@c4j.be>
2016-10-27 16:24:18 +0200

    index page with blog list, error page

--- routes/main.js
+++ routes/main.js
@@ -3,9 +3,7 @@ const Express = require('express');
 const Router = Express.Router();
 const Blog = require('./../lib/blog');
 
-Router.get('/', (req, res, next) => {
-  res.status(200).render('index', {name: req.query.name});
-});
+Router.get('/', Blog.getAllBlogs);
 
 Router.get('/newBlog', Blog.getNewBlog);
 Router.post('/newBlog', Blog.saveNewBlog);
