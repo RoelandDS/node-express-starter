b8e338f242f73af7a29bb2dbd5a664ff19b1f007
Roeland De Smedt <roeland.desmedt@c4j.be>
2016-10-27 11:33:51 +0200

    adding a route

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const Config = require('./config/development');
 
 var app = Express();
 
+app.get('/', (req, res, next) => {
+  res.status(200).send('Hello world!');
+});
+
 app.listen(Config.port, () => {
   console.log('app running on port => ', Config.port);
 });
